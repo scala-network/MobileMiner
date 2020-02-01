5549fc62abae686577373347ecec74c0da954322
Mas7erMind <info.crypto.franco@gmail.com>
2020-01-31 20:02:50 -0500

    Added Donate button to support the project

--- app/src/main/java/io/scalaproject/androidminer/AboutFragment.java
+++ app/src/main/java/io/scalaproject/androidminer/AboutFragment.java
@@ -3,6 +3,8 @@
 // Please see the included LICENSE file for more information.
 package io.scalaproject.androidminer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,17 +13,21 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Map;
 
 public class AboutFragment extends Fragment {
 
+    private Button bDonate;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_about, container, false  );
+
+        bDonate = view.findViewById(R.id.donate);
 
         TextView tvWebsite;
         TextView tvGithub;
@@ -52,6 +58,7 @@ public class AboutFragment extends Fragment {
 
         tvEmail.setText(Html.fromHtml(getString(R.string.emailLink)));
         tvEmail.setMovementMethod(LinkMovementMethod.getInstance());
+
         String cpuinfo = Config.read("CPUINFO");
         if(cpuinfo.isEmpty()) {
             try {
@@ -79,6 +86,15 @@ public class AboutFragment extends Fragment {
 
         tvFontAwesome.setText(Html.fromHtml(getString(R.string.FontAwesomeLink)));
         tvFontAwesome.setMovementMethod(LinkMovementMethod.getInstance());
+
+        bDonate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(getString(R.string.DonateURL));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
