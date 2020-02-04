dd6c03eef8a8f8d0e6a86a07bf5ceeffd9da4a6a
Mas7erMind <info.crypto.franco@gmail.com>
2020-02-04 06:50:39 -0500

    UI update to About page

--- app/src/main/java/io/scalaproject/androidminer/AboutFragment.java
+++ app/src/main/java/io/scalaproject/androidminer/AboutFragment.java
@@ -27,29 +27,13 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false  );
 
-        TextView tvWebsite;
-        TextView tvGithub;
-        TextView tvDiscord;
-        TextView tvEmail;
-        TextView tvSystemInfo;
-        TextView tvMine2gether;
-        TextView tvMonerominer;
-        TextView tvFontAwesome;
+        TextView tvSystemInfo, tvScala, tvMine2gether, tvMonerominer, tvFontAwesome;
 
         tvSystemInfo = view.findViewById(R.id.systemInfo);
+        tvScala = view.findViewById(R.id.ScalaURL);
         tvMine2gether = view.findViewById(R.id.Mine2getherURL);
         tvMonerominer = view.findViewById(R.id.MoneroMinerURL);
         tvFontAwesome = view.findViewById(R.id.FontAwesomeURL);
-
-        Button btnLink = view.findViewById(R.id.btnWebsite);
-        btnLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse(getResources().getString(R.string.websiteLink));
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
 
         Button btnGitHub = view.findViewById(R.id.btnGitHub);
         btnGitHub.setOnClickListener(new View.OnClickListener() {
@@ -71,12 +55,32 @@ public class AboutFragment extends Fragment {
             }
         });
 
-        Button btnEmail = view.findViewById(R.id.btnEmail);
-        btnEmail.setOnClickListener(new View.OnClickListener() {
+        Button btnMedium = view.findViewById(R.id.btnMedium);
+        btnMedium.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uri = Uri.parse(getResources().getString(R.string.emailLink));
-                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                Uri uri = Uri.parse(getResources().getString(R.string.mediumLink));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        Button btnTwitter = view.findViewById(R.id.btnTwitter);
+        btnTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(getResources().getString(R.string.twitterLink));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        Button btnTelegram = view.findViewById(R.id.btnTelegram);
+        btnTelegram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(getResources().getString(R.string.telegramLink));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
         });
@@ -99,6 +103,9 @@ public class AboutFragment extends Fragment {
         }
 
         tvSystemInfo.setText(cpuinfo);
+
+        tvScala.setText(Html.fromHtml(getString(R.string.ScalaLink)));
+        tvScala.setMovementMethod(LinkMovementMethod.getInstance());
 
         tvMine2gether.setText(Html.fromHtml(getString(R.string.Mine2getherLink)));
         tvMine2gether.setMovementMethod(LinkMovementMethod.getInstance());
